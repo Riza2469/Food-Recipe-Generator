1a4a2c79eece8b607379178dd255b54e52596be2
Riza2469 <rizatp2469@gmail.com>
2024-04-27 11:45:28 +0530

    new

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 from urllib.parse import quote_plus
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -12,9 +11,6 @@ from bson import json_util
 from flask import Response
 import json
 import requests
-import beam_search
-import top_sampling
-
 
 app = Flask(__name__)
 CORS(app)
@@ -30,35 +26,6 @@ recipes_collection = mongo_db['recipes']
 user_recipes_collection = mongo_db['user_recipes']
 user_favorites_collection = mongo_db['user_favorites']
 
-
-def load_model():
-    tokenizer = AutoTokenizer.from_pretrained("flax-community/t5-recipe-generation")
-    model = AutoModelForSeq2SeqLM.from_pretrained("flax-community/t5-recipe-generation")
-    generator = pipeline("text2text-generation", model=model, tokenizer=tokenizer)
-    return generator, tokenizer
-
-
-def generate_recipe(ingredients):
-    # Load the model globally for efficiency (consider caching for larger models)
-    generator, tokenizer = load_model()
-    all_ingredients = ", ".join(ingredients)
-    
-    # Generate recipe using chosen logic
-    if sampling_mode == "Beam Search":
-        generated = generator(all_ingredients, return_tensors=True, return_text=False, **beam_search.generate_kwargs)
-        outputs = beam_search.post_generator(generated, tokenizer)
-    elif sampling_mode == "Top-k Sampling":
-        generated = generator(all_ingredients, return_tensors=True, return_text=False, **top_sampling.generate_kwargs)
-        outputs = top_sampling.post_generator(generated, tokenizer)
-    output = outputs[0]
-    return output
-
-@app.route('/generate_recipe', methods=['POST'])
-def generate_recipe_api():
-    data = request.get_json()
-    ingredients = data["ingredients"]
-    recipe = generate_recipe(ingredients)
-    return jsonify(recipe)
 
 def fetch_unsplash_image_url(query, fallback_query=None):
     access_key = os.getenv('UNSPLASH_ACCESS_KEY')
@@ -426,5 +393,4 @@ def test():
     return jsonify({"message": "Test route is working!"}), 200
 
 if __name__ == '__main__':
-    sampling_mode = "Beam Search"  # or "Top-k Sampling"
     app.run(debug=True)
